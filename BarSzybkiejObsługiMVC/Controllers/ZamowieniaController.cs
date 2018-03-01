3daa33fa0b1d7951b04ba2ae298ff9c605cbee9c
asdator1213 <trrrrr01@gog.pl>
2018-03-01 12:38:43 +0100

    Widoki dla zamówien

---
+++ BarSzybkiejObsługiMVC/Controllers/ZamowieniaController.cs
@@ -0,0 +1,23 @@
+﻿using BarSzybkiejObsługiMVC.DAL;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace BarSzybkiejObsługiMVC.Controllers
+{
+    public class ZamowieniaController : Controller
+    {
+        private BarContext db = new BarContext();
+
+        public ActionResult Index()
+        {
+            var zamowienia = db.Zamowienia.ToList();
+            return View(zamowienia);
+        }
+
+        public ActionResult Szczegoly(int id)
+        {
+            var zamowienie = db.Zamowienia.Find(id);
+            return View(zamowienie);
+        }
+    }
+}
